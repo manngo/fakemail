010e062266f8f696744ccf5347b56dd4a7fadb45
manngo <mark@manngo.net>
2019-07-02 13:05:51 +1000

    Added Output Parameter for Scripts

--- shendmail.bat
+++ shendmail.bat
@@ -1,7 +1,12 @@
 @echo off
-set file="%dp0\mail.txt"
+if "%~1"=="" (
+	set file="%~dp0\mail.txt"
+) else (
+	set file="%~1"
+)
+
 echo ================================================>>"%file%"
 echo %date% %time%>>"%file%"
 echo ------------------------------------------------>>"%file%"
-echo .>>"%file%"
+echo.>>"%file%"
 more | find /v "" >> "%file%"
--- shendmail.sh
+++ shendmail.sh
@@ -2,6 +2,13 @@
 #	chmod 744 sendmail.sh
 DIR=$(dirname "$0");
 FILE="${DIR}/mail.txt"
+
+if [ -z "$1" ]; then
+	FILE="${DIR}/mail.txt"
+else
+	FILE="$1"
+fi
+
 echo ================================================ >> "$FILE"
 date >> "$FILE"
 echo ------------------------------------------------ >> "$FILE"
