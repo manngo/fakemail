266400a5f2d5f5875c42ba0d043b4cbf58d16195
manngo <mark@manngo.net>
2019-07-03 13:23:10 +1000

    Ready to Go
    Better Organised
    Compiled for both MacOS and Windows
    Accept Destination as Argument

---
+++ source/shendmail.cpp
@@ -0,0 +1,51 @@
+/*	Platform
+	================================================
+	This works as is on MacOS and Linux.
+	If you are using Windows the #ifdef below should
+	detect it, and the POSIX function realpath() is
+	faked with the Windows function _fullpath()
+	================================================ */
+	#ifdef _WIN32
+		#define realpath(N,R) _fullpath((R),(N),_MAX_PATH)
+	#endif
+
+//	Includes
+
+	#include <fstream>
+	#include <string>
+	#include <iostream>
+	#include <stdio.h>
+	#include <libgen.h>
+
+	using namespace std;
+
+//	main
+
+	int main(int argc, char *argv[]) {
+		//	CWD
+			char buffer[FILENAME_MAX];
+			realpath(argv[0],buffer);
+			string cwd=dirname(buffer);
+
+	//	cout<<cwd<<"\n";
+
+		//	Define Input
+			string input;
+
+		//	Define Output
+			string outputFile=cwd+"/mail.txt";
+			if(argc>1) outputFile=argv[1];
+			ofstream output;
+			output.open(outputFile,ofstream::out | ofstream::app | ofstream::binary);
+
+	//	cout<<outputFile<<"\n";
+
+		//	Do the thing …
+			while(getline(cin,input)) {
+				output<<input;
+				output<<"\n";
+			}
+			output<<"================================================\n\n";
+
+		return 0;
+	}
